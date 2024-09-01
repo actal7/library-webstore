37731a369f5cf08bf4c71162d35cb0a82becee85
actalian <ac.taloi7@gmail.com>
2024-09-02 01:35:30 +0300

    bust cache

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo 'testing cache bust';
+
 switch ($request) {
     case '/' :
         require __DIR__ . '/../src/controllers/BookController.php';
